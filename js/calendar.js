bff01127e545bee057f7aea2704367d462d7751b
linzinha <lindsey.m@posteo.net>
2023-12-05 00:23:15 +0000

    modified:   data/players.json 	modified:   index.html 	modified:   js/calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
     //December array
     const dec = []
     const winnerBox = ["Alium (#1933235)","Sheber (#2949208)","chocofungi (#2188317)",4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,1,2]
-    const prizeBox = ["Polka Pop", "resolution: be more adventurous","resolution: be sexier","druidic s'more",5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,1,2]
+    const prizeBox = ["Polka Pop", "resolution: be more adventurous","resolution: be sexier","druidic s'more","cold hi mein",6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,1,2]
     count = -4
 
     console.log(today)
